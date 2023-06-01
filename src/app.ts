425c70052ec40b459a7079b79f89fb5fa692089d
Farshid-997 <arnob.farshid@gmail.com>
2023-06-01 21:23:33 +0600

    [Done] route controller and interface model file

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
-// const port = 3000;
 
 app.use(cors())
 
@@ -9,8 +9,12 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// Application routes
+
+app.use('/api/v1/users/', usersRouter)
+
 //Testing
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
   res.send('Working Successfully')
 })
 
