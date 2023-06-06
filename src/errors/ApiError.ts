111c4c365d02fff80f1fd3f71af80a01d9bf8193
Farshid Ahsan <arnob.farshid@gmail.com>
2023-06-06 23:25:20 +0600

    added

---
+++ src/errors/ApiError.ts
@@ -0,0 +1,18 @@
+/* eslint-disable consistent-type-definitions */
+// eslint-disable-next-line consistent-type-definitions
+class ApiError extends Error {
+    statusCode: number;
+  
+    constructor(statusCode: number, message: string | undefined, stack = '') {
+      super(message);
+      this.statusCode = statusCode;
+      if (stack) {
+        this.stack = stack;
+      } else {
+        Error.captureStackTrace(this, this.constructor);
+      }
+    }
+  }
+  
+  export default ApiError;
+  
