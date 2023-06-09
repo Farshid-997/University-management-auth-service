3d1622f3bded1687c7b743cf3c8aa40ddd981434
Farshid Ahsan <arnob.farshid@gmail.com>
2023-06-10 01:56:16 +0600

    added filter and search

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,11 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
+import { paginationFields } from '../../../Constants/Pagination';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { academicSemesterFilterableFields } from './academicSemester.constants';
 import { IAcademicSemester } from './academicSemester.interface';
 import { AcademicSemesterService } from './academicSemester.service';
-import pick from '../../../shared/pick';
-import { paginationFields } from '../../../Constants/Pagination';
+
+
 
 const createSemester = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -24,25 +27,25 @@ const createSemester = catchAsync(
   }
 );
 
-const getAllSemester=catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-  
-    const paginationOptions=pick(req.query,paginationFields)
-  
-    const result =await  AcademicSemesterService.getAllSemesters( paginationOptions)
+const getAllSemesters = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, academicSemesterFilterableFields);
+  const paginationOptions = pick(req.query, paginationFields);
 
-    sendResponse<IAcademicSemester[]>(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Aademic semester retreive successfully!',
-      meta:result.meta,
-      data: result.data,
-    });
-    next();
-  }
+  const result = await AcademicSemesterService.getAllsemesters(
+    filters,
+    paginationOptions
+  );
 
-)
+  sendResponse<IAcademicSemester[]>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Semesters retrieved successfully !',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
 export const AcademicSemesterController = {
   createSemester,
-  getAllSemester
+  getAllSemesters
 };
--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,10 +1,18 @@
 import httpStatus from 'http-status';
+import { SortOrder } from 'mongoose';
 import ApiError from '../../../errors/ApiError';
-import { academicSemesterTitleCodeMapper } from './academicSemester.constant';
-import { IAcademicSemester } from './academicSemester.interface';
-import { AcademicSemester } from './academicSemester.model';
-import { IPpaginationOptions } from '../../../interfaces/pagination';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
 import { IGenericResponse } from '../../../interfaces/common';
+import { IPpaginationOptions } from '../../../interfaces/pagination';
+import {
+  academicSemesterSearchableFields,
+  academicSemesterTitleCodeMapper,
+} from './academicSemester.constants';
+import {
+  IAcademicSemester,
+  IAcademicSemesterFilters,
+} from './academicSemester.interface';
+import { AcademicSemester } from './academicSemester.model';
 
 const createSemester = async (
   payload: IAcademicSemester
@@ -16,23 +24,99 @@ const createSemester = async (
   return result;
 };
 
+const getAllsemesters = async (
+  filters: IAcademicSemesterFilters,
+  paginationOptions: IPpaginationOptions
+): Promise<IGenericResponse<IAcademicSemester[]>> => {
+  const { searchTerm, ...filtersData } = filters;
+
+  const andConditions = [];
+
+  if (searchTerm) {
+    andConditions.push({
+      $or: academicSemesterSearchableFields.map(field => ({
+        [field]: {
+          $regex: searchTerm,
+          $options: 'i',
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filtersData).length) {
+    andConditions.push({
+      $and: Object.entries(filtersData).map(([field, value]) => ({
+        [field]: value,
+      })),
+    });
+  }
 
 
-const getAllSemesters=async (paginationOptions:IPpaginationOptions):Promise<IGenericResponse<IAcademicSemester[]>>=>{
- const {page=1,limit=10}=paginationOptions;
- const skip=(page-1)*limit;
- const result= await AcademicSemester.find().sort().skip(skip).limit(limit);
- const total= await AcademicSemester.countDocuments();
- return {
-  meta:{
-page,
-limit,
-total
-  },
-  data:result
- }
-}
+
+  const { page, limit, skip, sortBy, sortOrder } =
+    paginationHelpers.calculatePagination(paginationOptions);
+
+  const sortConditions: { [key: string]: SortOrder } = {};
+
+  if (sortBy && sortOrder) {
+    sortConditions[sortBy] = sortOrder;
+  }
+  const whereConditons =
+    andConditions.length > 0 ? { $and: andConditions } : {};
+
+  const result = await AcademicSemester.find(whereConditons)
+    .sort(sortConditions)
+    .skip(skip)
+    .limit(limit);
+
+  const total = await AcademicSemester.countDocuments();
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
+const getSingleSemester = async (
+  id: string
+): Promise<IAcademicSemester | null> => {
+  const result = await AcademicSemester.findById(id);
+  return result;
+};
+
+const updateSemester = async (
+  id: string,
+  payload: Partial<IAcademicSemester>
+): Promise<IAcademicSemester | null> => {
+  if (
+    payload.title &&
+    payload.code &&
+    academicSemesterTitleCodeMapper[payload.title] !== payload.code
+  ) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid Semester Code');
+  }
+
+  const result = await AcademicSemester.findOneAndUpdate({ _id: id }, payload, {
+    new: true,
+  });
+  return result;
+};
+
+const deleteSemester = async (
+  id: string
+): Promise<IAcademicSemester | null> => {
+  const result = await AcademicSemester.findByIdAndDelete(id);
+  return result;
+};
+
 export const AcademicSemesterService = {
   createSemester,
-  getAllSemesters
+  getAllsemesters,
+  getSingleSemester,
+  updateSemester,
+  deleteSemester,
 };
